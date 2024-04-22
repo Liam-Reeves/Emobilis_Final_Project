2a96e02d5acf1eb791f8b51a7c715eef49e07823
Liam Reeves <wesleyliam005@gmail.com>
2024-04-22 16:21:06 +0300

    first commit

---
+++ app/src/main/java/net/ezra/ui/home/HomeScreen.kt
@@ -0,0 +1,95 @@
+package net.ezra.ui.home
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import net.ezra.navigation.ROUTE_ABOUT
+import net.ezra.navigation.ROUTE_HOME
+import net.ezra.navigation.ROUTE_SERVICES
+import net.ezra.navigation.ROUTE_CONTACT
+import net.ezra.navigation.ROUTE_PRODUCTS
+import net.ezra.R
+import net.ezra.navigation.ROUTE_ADD_STUDENTS
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(navController: NavHostController) {
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(text = "Top App Bar")
+                },
+                navigationIcon = {
+                    IconButton(onClick = {}) {
+                        Icon(Icons.Filled.ArrowBack, "backIcon")
+                    }
+                },
+            )
+        }, content = {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color(0xff8d6e63)),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+
+                Text(
+                    text = "Content of the page",
+                    fontSize = 30.sp,
+                    color = Color.White
+                )
+
+
+
+
+
+
+
+            }
+
+        })
+
+
+}
+
+@Composable
+fun Greeting_Section(navController: NavHostController){
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewLight() {
+    HomeScreen(rememberNavController())
+}
+
